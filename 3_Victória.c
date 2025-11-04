adee5125730b33eb023db8266cc7ba58aa28bc40
vbraynm <victoria.braynm18@gmail.com>
2025-11-04 20:26:01 -0300

    Update 3_Victória.c

--- 3_Victória.c
+++ 3_Victória.c
@@ -1,17 +1,9 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
 
 int main()
 {
     int numero;
-    int soma;
+    int soma  = 0;
     do {
         printf("Digite um número: ");
         scanf("%i", &numero);
@@ -20,7 +12,7 @@ int main()
         }
     } while (numero >= 0);
     
-    printf("Você digitou um número negativo, a leitura foi encerrada.\n");
+    printf("A leitura foi encerrada porque você digitou um numero negativo.\n");
     printf("A soma de todos os números positivos digitados é: %i", soma);
     
     return 0;    
