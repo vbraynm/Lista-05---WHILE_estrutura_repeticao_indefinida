fc4a9c1ad57632cef0fab06076fd6c429253b1a7
vbraynm <victoria.braynm18@gmail.com>
2025-11-04 17:30:50 -0300

    Add files via upload

---
+++ exercício extra.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+ int main(){
+    
+   int i = 10;
+
+   while (i > -1)
+   {
+      printf("%i\n", i); 
+      printf("Ainda nao terminou!\n");
+      i--;
+   }
+
+  printf("Terminou!\n");
+
+  return 0;
+ }
+
